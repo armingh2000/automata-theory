244c7e55a771721007d83df965038de3804d5561
armingh2000 <armingholampour71@gmail.com>
2020-06-06 17:49:40 -0400

    tape class

--- tm/tm.py
+++ tm/tm.py
@@ -12,3 +12,28 @@ class tm_state:
         self.transitions[tape_read].append((state2, tape_write, move_direction))
 
 
+class tape:
+
+
+    def __init__(self):
+        self.tape = []
+        self.current_index = 0
+
+    def clear(self):
+        self.tape = 0
+        self.current_index = 0
+
+    def add_word(self, word):
+        self.tape = ['#'] + list(word) + ['#']
+        self.current_index = 1
+
+    def perform_transition(self, transition):
+        self.tape[self.current_index] = transition[1]
+        self.current_index = self.current_index + 1 if transition(2) == 'r' else self.transition - 1
+
+    def show(self):
+        return self.tape[:]
+
+    def current_letter(self):
+        return self.tape[self.current_index]
+
